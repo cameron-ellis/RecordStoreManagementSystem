570f8a3da7b7b889807744ff6ae5edf83ca69699
cmhg3g <cmhg3g@umsystem.edu>
2022-05-12 20:56:15 -0500

    Created Menu Files

---
+++ Menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+#include "Creator.h"
+#include "InterfaceCreator.h"
+#include "Inventory.h"
+#include "Interface.h"
+#include "Customer.h"
+#include "Employee.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits>
